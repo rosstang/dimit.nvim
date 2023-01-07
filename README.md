42b2c0bceae83001b82e82963f7f6c36c2188df8
Ross Tang <ph.tchaa@gmail.com>
2023-01-07 15:33:02 +0800

    Initial version of dimmer
    Signed-off-by: Ross Tang <ph.tchaa@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # dimmer.nvim
-A neovim plugin to dim inactive windows
+A neovim plugin to dim inactive windows. 
+
+It is mainly a rewrite of jceb/blinds.nvim, which uses lua and makes configuration easier, and work with package manager.
+
+![Dimmer animation](dimmer-nvim.gif)
+
+# Installation
+- Install it with packer (or any other package manager)
+```lua
+use {
+    "rosstang/dimmer.nvim",
+    config = function()
+        require("dimmer").setup()
+    end,
+}
+```
+
+# Configuration
+- default config
+```lua
+M.config = {
+    bgcolor = "#303030",
+    highlight_group = "Dimmer",
+    auto_dim = true,
+    dim_elements = {
+        "ColorColumn",
+        "CursorColumn",
+        "CursorLine",
+        "CursorLineFold",
+        "CursorLineNr",
+        "CursorLineSign",
+        "EndOfBuffer",
+        "FoldColumn",
+        "LineNr",
+        "NonText",
+        "Normal",
+        "SignColumn",
+        "VertSplit",
+        "Whitespace",
+        "WinBarNC",
+        "WinSeparator",
+    },
+}
+```
+
+- setup() can be called multiple times to change configuration,
+```lua
+require("dimmer").setup({
+    bgcolor = "FF0000",         -- change the dim color to red
+})
+```
+
+- In case the current windows is also dimmed (not undimmed), you can call :DimmerNow to redo the dimming.
+
---
+++ lua/dimmer/init.lua
@@ -0,0 +1,69 @@
+local get_highlight_value = function(dim_elements, hlgroup)
+    return table.concat(dim_elements, ":" .. hlgroup .. ",") .. ":" .. hlgroup
+end
+
+local merge_tb = function(default, new)
+    return vim.tbl_deep_extend("force", default, new)
+end
+
+local M = {}
+
+M.config = {
+    bgcolor = "#303030",
+    highlight_group = "Dimmer",
+    auto_dim = true,
+    dim_elements = {
+        "ColorColumn",
+        "CursorColumn",
+        "CursorLine",
+        "CursorLineFold",
+        "CursorLineNr",
+        "CursorLineSign",
+        "EndOfBuffer",
+        "FoldColumn",
+        "LineNr",
+        "NonText",
+        "Normal",
+        "SignColumn",
+        "VertSplit",
+        "Whitespace",
+        "WinBarNC",
+        "WinSeparator",
+    },
+}
+
+M.dim_inactive = function()
+    local config = M.config
+    vim.api.nvim_set_hl(0, config.highlight_group, { bg = config.bgcolor })
+    local current = vim.api.nvim_get_current_win()
+    local win_cfg = vim.api.nvim_win_get_config(current)
+    if win_cfg["relative"] ~= "" then
+        -- ignore floating window
+        return
+    end
+    local dim_value = get_highlight_value(config.dim_elements, config.highlight_group)
+    for _, w in pairs(vim.api.nvim_list_wins()) do
+        local winhighlights = current == w and "" or dim_value
+        vim.api.nvim_win_set_option(w, "winhighlight", winhighlights)
+    end
+end
+
+M.setup = function(opts)
+    opts = opts == nil and {} or opts
+    M.config = merge_tb(M.config, opts)
+    M.dim_inactive()
+    vim.api.nvim_create_user_command("DimmerNow", M.dim_inactive, {})
+    if not M.config.auto_dim then
+        return
+    end
+    if M.autocmd ~= nil then
+        vim.api.nvim_del_autocmd(M.autocmd)
+    end
+    M.autocmd = vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter" }, {
+        callback = function()
+            M.dim_inactive()
+        end,
+    })
+end
+
+return M
